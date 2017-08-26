54771f4b2d2c536fbe979c18a526e73df22d0346
Manaswini <manaswinijarugula@gmail.com>
2017-08-26 11:12:16 -0500

    added autoit to uplaod a file

--- src/com/training/selenium/AutoITExample.java
+++ src/com/training/selenium/AutoITExample.java
@@ -15,12 +15,30 @@ public class AutoITExample {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("http://toolsqa.wpengine.com/automation-practice-form");
 		driver.manage().window().maximize();
-		 
+		
+		
+		//Click Browse button in the web page 
 	    driver.findElement(By.id("photo")).click(); 
-	    Runtime.getRuntime().exec("D:\\AutoIt\\AutoItTest.exe");
+	    //To run Executable file created by Auto IT 
+	    Runtime.getRuntime().exec("C:\\Users\\BhagyaSri\\Desktop\\ss class\\upload.exe");
  
 	    Thread.sleep(5000); 
+	    
+	    //to Close the current browser window which is associated with driver instance 
 	    driver.close();
+	    
+	    
+	    /*
+	     1. Install Auto It tool and editor 
+	     2. Write auto It script in the scite editor 
+	     3. Save the file with .au3 extension
+	     4. Compile the file by doing right click on .au3 file 
+	     5. give the path of .exe file in the above runtime command
+	     	     
+	     */
+	    
+	    
+	    
 		
 	}
 
